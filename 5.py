f75077f35683beba1026332c47dcbf95a97e434d
Emil Vaughan <emil79@gmail.com>
2013-05-18 22:12:59 +0100

    Add 'human' solution to Problem 5. Is this cheating?

---
+++ 5.py
@@ -0,0 +1,4 @@
+# I'm not sure this one needs a program, perhaps a prime generator.
+# Take the product of, for every prime p, the highest power of p that
+# is less than 21.
+print 2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19
